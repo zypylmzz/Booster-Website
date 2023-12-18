a32322abbaf17c9dc5df1d003ec30a50ace7c7f5
Zeynep Yılmaz <75902079+zypylmzz@users.noreply.github.com>
2023-12-18 17:28:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Smooth scrolling
 $('a[href^="#"]').on("click", function (e) {
   e.preventDefault();
 
-  var target = this.hash;
-  var $target = $(target);
+  let target = this.hash;
+  let $target = $(target);
 
   $("html, body")
     .stop()
@@ -20,11 +19,11 @@ $('a[href^="#"]').on("click", function (e) {
 });
 
 // Hide/Show Navbar on scroll
-var lastScrollTop = 0;
-var navbar = $(".navbar");
+let lastScrollTop = 0;
+let navbar = $(".navbar");
 
 $(window).scroll(function (event) {
-  var st = $(this).scrollTop();
+  let st = $(this).scrollTop();
 
   if (st > lastScrollTop) {
     // Downscroll code
@@ -37,47 +36,49 @@ $(window).scroll(function (event) {
   lastScrollTop = st;
 });
 
-$(document).ready(function () {
-  // Owl Carousel
-  $(".studio-slider-owl").owlCarousel({
-    loop: true,
-    nav: false,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    smartSpeed: 450,
-    margin: 20,
-    responsive: {
-      0: { items: 1 },
-      768: { items: 2 },
-      991: { items: 3 },
-      1200: { items: 3 },
-      1920: { items: 3 },
-    },
-  });
-
-  // Swiper
-  var mySwiper = new Swiper(".studio-slider-swiper", {
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    slidesPerView: 1,
-    spaceBetween: 20,
-    breakpoints: {
-      768: { slidesPerView: 2 },
-      991: { slidesPerView: 3 },
-      1200: { slidesPerView: 3 },
-      1920: { slidesPerView: 3 },
-    },
-  });
+// Owl Carousel Initialization
+$(".studio-slider-owl").owlCarousel({
+  loop: true,
+  nav: false,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  smartSpeed: 450,
+  margin: 20,
+  responsive: {
+    0: { items: 1 },
+    768: { items: 2 },
+    991: { items: 3 },
+    1200: { items: 3 },
+    1920: { items: 3 },
+  },
 });
 
-//Counter
+// Swiper
+let mySwiper = new Swiper(".studio-slider-swiper", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    768: { slidesPerView: 2 },
+    991: { slidesPerView: 3 },
+    1200: { slidesPerView: 3 },
+    1920: { slidesPerView: 3 },
+  },
+});
+
+// Bu satırları ekleyerek navigation (kaydırma tuşları) ve pagination (sayfalama noktaları) öğelerini kaldırabilirsiniz
+mySwiper.navigation.destroy();
+mySwiper.pagination.destroy();
+
+// Counter
 document.addEventListener("DOMContentLoaded", function () {
   const counters = document.querySelectorAll(".counter");
   const speed = 5000;
@@ -101,38 +102,55 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-$(document).ready(function () {
-  $(".studio-slider").owlCarousel({
-    items: 12,
-    loop: true,
-    margin: 20,
-    nav: true,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    navText: [
-      "<i class='bi bi-chevron-left'></i>",
-      "<i class='bi bi-chevron-right'></i>",
-    ],
-    responsive: {
-      0: {
-        items: 1,
-      },
-      768: {
-        items: 2,
-      },
-      992: {
-        items: 3,
-      },
+// Owl Carousel Initialization
+$(".studio-slider").owlCarousel({
+  items: 12,
+  loop: true,
+  margin: 20,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  navText: [],
+  responsive: {
+    0: {
+      items: 1,
     },
+    768: {
+      items: 2,
+    },
+    992: {
+      items: 3,
+    },
+  },
+});
+
+// Testimonial Box
+document.querySelectorAll(".testmonial-box .single-box").forEach((box) => {
+  let p = box.querySelector("p");
+  let linkedinButton = box.querySelector(".linkedin-button");
+
+  p.style.display = "none";
+
+  box.addEventListener("click", function () {
+    p.style.display = p.style.display === "none" ? "block" : "none";
+
+    if (box.classList.contains("enlarged")) {
+      box.classList.remove("enlarged");
+    } else {
+      document
+        .querySelectorAll(".testmonial-box .single-box")
+        .forEach((otherBox) => {
+          otherBox.classList.remove("enlarged");
+        });
+
+      box.classList.add("enlarged");
+    }
+  });
+
+  box.addEventListener("mouseleave", function () {
+    if (box.classList.contains("enlarged")) {
+      box.classList.remove("enlarged");
+    }
   });
 });
 
-function showText(card) {
-  var hiddenText = card.querySelector(".hidden-text");
-  hiddenText.style.display = "block";
-}
-
-function hideText(card) {
-  var hiddenText = card.querySelector(".hidden-text");
-  hiddenText.style.display = "none";
-}
